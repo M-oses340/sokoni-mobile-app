7319af69df6c636c3a0c762d70353e92bd45c2fb
Moses Omwa <mosesomwa7@gmail.com>
2025-12-29 17:57:06 +0300

    cart routes & controllers added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router' // <--- Add this
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -10,10 +15,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
- <StrictMode>
+  <StrictMode>
+    <QueryClientProvider client={queryClient}> {/* Provide React Query context */}
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
+        <BrowserRouter> {/* Provide Routing context to App */}
+          <App />
+        </BrowserRouter>
       </ClerkProvider>
-    </StrictMode>,
+    </QueryClientProvider>
+  </StrictMode>,
 )
