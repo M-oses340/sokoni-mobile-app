4786a0e270615b00e9e517cc0837614e5a883a8e
Moses Omwa <mosesomwa7@gmail.com>
2026-01-06 07:57:38 +0300

    tiny fixes

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// /api/index.js (The only file you need in the root api folder)
+import app from "../backend/src/server.js";
+export default app;
--- inngest.js
+++
@@ -1,7 +0,0 @@
-import { serve } from "inngest/express";
-import { inngest, functions } from "../backend/src/config/inngest.js";
-
-export default serve({
-  client: inngest,
-  functions,
-});
